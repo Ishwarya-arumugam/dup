31e84707af35abdfe57e3150cb928ba3c26b6ffe
Ishwarya-arumugam <ishwaryaa44@gmail.com>
2016-07-06 10:18:01 +0530

    Create dup.c

---
+++ dup.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[100];
+int i=0,k=0,j;
+clrscr();
+scanf("%s",a);
+j=strlen(a);
+while(k<j)
+{
+if(a[i]==a[i+1])
+i++;
+else
+a[k]=a[i];
+i++;
+k++;
+}
+for(i=0;i<k;i++)
+{
+printf("%c",a[i]);
+}
+getch();
+}
